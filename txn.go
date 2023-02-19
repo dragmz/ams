093af84b64f6390f69775e77be6bef3b28fa1cca
Marcin Zawiejski <dragmz@gmail.com>
2023-02-19 23:09:38 +0100

    display extra txn details for pay and axfer

--- txn.go
+++ txn.go
@@ -12,6 +12,23 @@ func FormatTxn(txn types.Transaction) string {
 
 	out.WriteString(fmt.Sprintf("Type: %s\n", txn.Type))
 	out.WriteString(fmt.Sprintf("Sender: %s\n", txn.Sender))
+
+	switch txn.Type {
+	case types.PaymentTx:
+		out.WriteString(fmt.Sprintf("Receiver: %s\n", txn.Receiver))
+		out.WriteString(fmt.Sprintf("Amount: %d microALGO\n", txn.Amount))
+		if !txn.CloseRemainderTo.IsZero() {
+			out.WriteString(fmt.Sprintf("Close remainer to: %s\n", txn.CloseRemainderTo.String()))
+		}
+
+	case types.AssetTransferTx:
+		out.WriteString(fmt.Sprintf("Receiver: %s\n", txn.AssetReceiver))
+		out.WriteString(fmt.Sprintf("Amount: %d base units of ASA\n", txn.AssetAmount))
+		if !txn.AssetCloseTo.IsZero() {
+			out.WriteString(fmt.Sprintf("Close to: %s\n", txn.AssetCloseTo.String()))
+		}
+	}
+
 	out.WriteString(fmt.Sprintf("Group Id: %s\n", txn.Group))
 	out.WriteString(fmt.Sprintf("Validity: %d..%d (%d rounds)\n", txn.FirstValid, txn.LastValid, txn.LastValid-txn.FirstValid+1))
 
